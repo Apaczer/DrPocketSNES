65f17943c177559568af9d2bb9a1001ecd8f0f35
Apaczer <94932128+Apaczer@users.noreply.github.com>
2022-10-26 16:35:53 +0200

    add compil. script
    for miyoo toolchain

---
+++ miyoo_CI.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export SDKC=/opt/miyoo/usr
+export PATH=$PATH:${SDKC}/bin
+export TARGETMACH=arm-linux
+export BUILDMACH=i686-pc-linux-gnu
+export CROSS=arm-linux
+export CC=${CROSS}-gcc
+export LD=${CROSS}-ld
+export AS=${CROSS}-as
+export CXX=${CROSS}-g++
+export STRIP=${CROSS}-strip
